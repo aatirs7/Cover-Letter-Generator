463ea1b10488a45edd019ad38f105b938733a0df
aatirs7 <84591472+aatirs7@users.noreply.github.com>
2022-10-11 16:37:12 -0400

    Add files via upload

---
+++ CV Update Script.ps1
@@ -0,0 +1,66 @@
+﻿Copy-Item "C:\Users\aatir\OneDrive\Desktop\Cover Letters\cv test doc.docx" -Destination "C:\Users\aatir\OneDrive\Desktop\Cover Letters\cv test doc copy.docx"
+#$template = Read-Host "Please enter file path of CV Template"
+$Company = Read-Host "Please enter the company name"
+$title = Read-Host "Please enter the postition title"
+$description = Read-Host "Please describe your interest in this company"
+
+# Start Word Object
+$Word = New-Object -ComObject Word.Application
+
+
+# Open Word doc
+$OpenFile = $Word.Documents.Open("C:\Users\aatir\OneDrive\Desktop\Cover Letters\cv test doc copy.docx")
+#$template
+
+
+# Get the content of the doc
+$Content = $OpenFile.Content
+# My name is $name and the reason is $reason.
+
+# New variable for new text and variables to to replace the ones from the doc.
+$newText = ""
+$companyName = $Company
+$date =  Get-Date -Format " MM/dd/yyyy"
+$title = $title
+$description = $description
+
+# Store the current text in the var
+$newText = $Content.Text
+
+# Replace the template vars with the new values
+$newText = $newText  -replace '\$companyName', $companyName
+$newText = $newText  -replace '\$title', $title
+$newText = $newText  -replace '\$datee', $date
+$newText = $newText  -replace '\$description', $description
+# Make the modified text the new content and Save
+$Content.Text = $newText
+$OpenFile.Save()
+$Word.Quit()
+$path = "C:\Users\aatir\OneDrive\Desktop\Cover Letters\cv test doc copy.docx"
+
+
+
+$wd = New-Object -ComObject Word.Application
+
+Get-ChildItem -Path $path -Include *.doc, *.docx -Recurse |
+
+ForEach-Object {
+
+$doc = $wd.Documents.Open($_.Fullname)
+
+$pdf = $_.FullName -replace $_.Extension, '.pdf'
+
+$doc.ExportAsFixedFormat($pdf,17,$false,0,3,1,1,0,$false, $false,0,$false, $true)
+
+$doc.Close()
+
+}
+
+$wd.Quit()
+$cv = $companyName + " Cover Letter.pdf"
+
+Start-Sleep -Seconds .5
+Rename-Item "C:\Users\aatir\OneDrive\Desktop\Cover Letters\cv test doc copy.pdf" $cv 
+Remove-Item "C:\Users\aatir\OneDrive\Desktop\Cover Letters\cv test doc copy.docx"
+
+Write-Host "Process Complete"
